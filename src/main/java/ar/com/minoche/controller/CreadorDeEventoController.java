50aae24c0eea90b8c4b112c113676f223595ba64
ElvioCondori <>
2020-11-16 21:25:25 -0300

    Agregado de @RequestMapping en capa controller

--- src/main/java/ar/com/minoche/controller/CreadorDeEventoController.java
+++ src/main/java/ar/com/minoche/controller/CreadorDeEventoController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping("/creadorDeEvento")
 public class CreadorDeEventoController {
     
     @Autowired
